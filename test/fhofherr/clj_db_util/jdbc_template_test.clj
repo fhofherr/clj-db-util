4fe0938e1bf465ac3858f30664cae5ca76246dc8
Ferdinand Hofherr <mail@ferdinandhofherr.de>
2014-12-22 15:15:36 +0100

    Test subselects with named parameters.

--- test/fhofherr/clj_db_util/jdbc_template_test.clj
+++ test/fhofherr/clj_db_util/jdbc_template_test.clj
@@ -6,16 +6,29 @@
 
 
 (deftest named-parameters
-  (let [stmt "SELECT 1 AS result FROM dual WHERE 1 = :number"]
-    (is (= {:result 1}
-         (t/query-str h2
-                      (test-db/h2-private-in-memory)
-                      stmt
-                      :params {:number 1}
-                      :result-set-fn first)))
-    (is (nil?
-         (t/query-str h2
-                      (test-db/h2-private-in-memory)
-                      stmt
-                      :params {:number 2}
-                      :result-set-fn first)))))
+  (testing "simple statements"
+    (let [stmt "SELECT 1 AS result FROM dual WHERE 1 = :number"]
+      (is (= {:result 1}
+             (t/query-str h2
+                          (test-db/h2-private-in-memory)
+                          stmt
+                          :params {:number 1}
+                          :result-set-fn first)))
+      (is (nil?
+            (t/query-str h2
+                         (test-db/h2-private-in-memory)
+                         stmt
+                         :params {:number 2}
+                         :result-set-fn first)))))
+  (testing "subselects"
+    (let [stmt "SELECT 1 AS result
+                FROM dual
+                WHERE 1 = :number
+                  AND EXISTS (SELECT 1 FROM DUAL WHERE 2 = :another-number)"]
+      (is (= {:result 1}
+             (t/query-str h2
+                          (test-db/h2-private-in-memory)
+                          stmt
+                          :params {:number 1
+                                   :another-number 2}
+                          :result-set-fn first))))))
