d911822dbdd5ba4768bdcff977387aab34ccb32d
Ferdinand Hofherr <mail@ferdinandhofherr.de>
2016-03-05 11:54:04 +0100

    Update dependencies.

--- project.clj
+++ project.clj
@@ -10,7 +10,7 @@
                  [org.clojure/java.jdbc "0.4.2"]
                  [org.clojure/tools.logging "0.3.1"]
                  [com.zaxxer/HikariCP "2.4.3"]
-                 [org.flywaydb/flyway-core "3.2.1"]]
+                 [org.flywaydb/flyway-core "4.0"]]
   :test-selectors {:default (complement :integration)
                    :integration :integration}
   :profiles  {:dev  {:source-paths ["dev"]
@@ -20,11 +20,11 @@
                                [lein-cloverage "1.0.6"]]
                      :codox {:namespaces [#"^fhofherr\.clj-db-util\."]
                              :metadata {:doc/format :markdown}}
-                     :dependencies [[environ "1.0.1"
+                     :dependencies [[environ "1.0.2"
                                      :exclusions [org.clojure/clojure]]
                                     [org.clojure/tools.namespace "0.2.10"]
-                                    [org.slf4j/slf4j-api "1.7.13"]
-                                    [org.slf4j/slf4j-simple "1.7.13"]
-                                    [com.h2database/h2 "1.4.190"]]}
-              :postgres {:dependencies [[org.postgresql/postgresql "9.4-1206-jdbc42"]]}
-              :h2 {:dependencies [[com.h2database/h2 "1.4.190"]]}})
+                                    [org.slf4j/slf4j-api "1.7.18"]
+                                    [org.slf4j/slf4j-simple "1.7.18"]
+                                    [com.h2database/h2 "1.4.191"]]}
+              :postgres {:dependencies [[org.postgresql/postgresql "9.4.1208"]]}
+              :h2 {:dependencies [[com.h2database/h2 "1.4.191"]]}})
